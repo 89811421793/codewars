89e1fae6a3af6bbcb24c957c1f609fc71d0efd9e
Maksim Dorogoi <monarch2013@mail.ru>
2023-06-13 20:52:36 +1100

    position of a letter in the alphabet

---
+++ kata1.js
@@ -0,0 +1,6 @@
+function position(letter){
+//Write your own Code!
+  let alphabet = " abcdefghijklmnopqrstuvwxyz";
+  let pos = alphabet.indexOf(letter);
+  return `Position of alphabet: ${pos}`;
+}
