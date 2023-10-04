424837901daca212e65acd5578a90649b0a3c994
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-04 22:27:54 +1100

    Shifty Closures

---
+++ 8kyu/Shifty Closures.js
@@ -0,0 +1,12 @@
+/*var name_one = 'Abe';
+var greet_abe = function() {
+  return "Hello, " + name_one + '!';
+};
+var name_two = 'Ben';
+var greet_ben = function() {
+  return "Hello, " + name_two + '!';
+};
+*/
+
+let greet_abe = () => "Hello, Abe!";    // function() {return "Hello,Abe!"}
+let greet_ben = () => "Hello, Ben!";    // function() {return "Hello, Ben!"}
