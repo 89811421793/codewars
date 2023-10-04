0c51b131ba72c1dcd8535a15b9c11dfdede47117
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-04 22:46:33 +1100

    Max Headroom and JavaScript style

---
+++ 8kyu/Max Headroom and JavaScript style.js
@@ -0,0 +1,16 @@
+/*function getMax1() {
+  var max = {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2(){
+  return {
+    name: 'Max Headroom'
+  }
+}
+*/
+
+// let getMax1 = getMax2 = function() {return {name: 'Max Headroom'} }
+let getMax1 = getMax2 = () => ({name: 'Max Headroom'}) 
