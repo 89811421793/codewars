348bc93dcddac1f522baf2bcf9bf5bd64835d3f0
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-07 19:33:47 +1100

    Grasshopper - Array Mean

--- 8kyu/Grasshopper - Array Mean.js
+++ 8kyu/Grasshopper - Array Mean.js
@@ -1,14 +1,13 @@
 var findAverage = function (nums) {
-  // Code here
-  let sum = 0;
-  let i = 0;
-  while (i < nums.length) {
-    sum += nums[i];
-    i += 1; //or i++
-  }
-
-  let average = sum / nums.length;
-  return average;
-};
-
-// const findAverage = nums => nums.reduce((a, b) => a + b) / nums.length;
+    // Code here
+    let sum = 0, i = 0;
+    while (i < nums.length) {
+      sum += nums[i];
+      i += 1; //or i++
+    }
+  
+    let average = sum / nums.length;
+    return average;
+  };
+  
+  // const findAverage = nums => nums.reduce((a, b) => a + b) / nums.length;
