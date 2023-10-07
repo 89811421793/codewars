024a1e06f1cac9c4de953613ea7bdf1f1bc50ec6
Maksim <104402088+89811421793@users.noreply.github.com>
2023-10-07 17:54:52 +1100

    Update Take an Arrow to the knee.js

--- 8kyu/Take an Arrow to the knee.js
+++ 8kyu/Take an Arrow to the knee.js
@@ -1,3 +1,13 @@
 var ArrowFunc = function(arr) {
     return arr.map( num => String.fromCharCode(num) ).join(''); //Complete this function
   }
+
+/* using the for loop:
+var ArrowFunc = function (arr) {
+  let res = "";
+  for (let i = 0; i < arr.length; i++) {
+    res += String.fromCharCode(arr[i]);
+  }
+  return res;
+};
+*/
