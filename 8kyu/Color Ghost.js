2fbf2eec5d5923099e2318bf0559353568cebe96
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-07 17:03:15 +1100

    Color Ghost

--- 8kyu/Color Ghost.js
+++ 8kyu/Color Ghost.js
@@ -8,3 +8,14 @@ var Ghost = function() {
     this.color = color[randomNum]
 };
 */
+
+/*
+class Ghost {
+  constructor(){
+    this.colors = ["white", "yellow", "purple", "red"]
+  }
+  get color(){
+    return this.colors[Math.floor(Math.random() * this.colors.length)]
+  }
+};
+*/
