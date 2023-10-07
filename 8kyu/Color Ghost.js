f575f2aca2f963908f50b45f2c0cb2d044052fac
Maksim <104402088+89811421793@users.noreply.github.com>
2023-10-07 17:01:07 +1100

    Update Color Ghost.js

--- 8kyu/Color Ghost.js
+++ 8kyu/Color Ghost.js
@@ -1,4 +1,10 @@
 var Ghost = function() {
     this.color = ['white','yellow','purple','red'][Math.floor(Math.random()*4)]
    };
-   
+
+/*var Ghost = function() {
+    var  color = ["white", "yellow", "purple", "red"]
+    var randomNum = Math.floor(Math.random() * 4)
+    this.color = color[randomNum]
+};
+*/
