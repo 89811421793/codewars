1e1b377bc4722eb44499eec9d83cb8098c520bfd
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-07 22:18:19 +1100

    A bugs trilogy (ep.1)

--- 8kyu/A bugs trilogy(ep1).js
+++ 8kyu/A bugs trilogy(ep1).js
@@ -6,3 +6,10 @@ function yourFutureCareer() {
 }
 
 yourFutureCareer();
+
+/*
+const yourFutureCareer = () => {
+  let career = Math.random();
+  return `${ career <= 0.32 ? 'FrontEnd' : (career <= 0.65 ? 'BackEnd' : 'Full-Stack') } Developer`;
+}
+*/
