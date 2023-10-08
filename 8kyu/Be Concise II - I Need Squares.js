384f16ea79d389f463559c873760f4380349a83b
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-08 22:22:13 +1100

    Be Concise II - I Need Squares

--- 8kyu/Be Concise II - I Need Squares.js
+++ 8kyu/Be Concise II - I Need Squares.js
@@ -1,3 +1,16 @@
-const squaresOnly = function(array) {
-    return array.filter(number => Number.isInteger(Math.sqrt(number)))
-  }
+const squaresOnly = function (array) {
+  return array.filter((number) => Number.isInteger(Math.sqrt(number)));
+};
+
+/*
+function squaresOnly(a) {
+  return a.filter(x => x ** 0.5 % 1 == 0);
+}
+*/
+
+/*let squaresOnly=a=>a.filter(x=>!(x**.5%1))*/
+
+/*var squaresOnly = array => array.filter(number => Math.sqrt(number) % 1 === 0);*/
+
+/*const squaresOnly = array =>
+  array.filter(val => Number.isInteger(val ** 0.5));*/
