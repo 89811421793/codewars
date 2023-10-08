01b0b9e7280410dba3888ca2a8a65d5b51b5ab66
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-08 21:11:54 +1100

    Improving Math.round(x)

---
+++ 8kyu/Improving Math.round(x).js
@@ -0,0 +1,10 @@
+Math.roundTo = function (number, precision) {
+    // TODO: Program Me
+    return Math.round(number * (10 ** precision)) / (10 ** precision);
+    // or: return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision);
+  }
+  
+  /*Math.roundTo = function (number, precision) {
+   return +(number).toFixed(precision)
+  }
+  */
