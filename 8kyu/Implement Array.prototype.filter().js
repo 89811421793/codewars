67b8f4fca0f048b3a4a6aa33127176d92e1e2203
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-08 21:44:45 +1100

    Implement Array.prototype.filter()

--- 8kyu/Implement Array.prototype.filter().js
+++ 8kyu/Implement Array.prototype.filter().js
@@ -1,12 +1,45 @@
-Array.prototype.filter = function (func) {
-    //nothing here, you will feel pretty awesome to write down by yourself
-    //have fun :)
+Array.prototype.filter = function(fnc) {
     let arr = [];
-      for(let i = 0; i < this.length; i++) {
-          if(func(this[i])) {
-              arr.unshift(this[i]);          // .push(this[i]) as well
-          }
-      }
-      return arr;
+    for(let i = 0; i < this.length; i++) {
+        if(fnc(this[i])) {
+            arr.push(this[i]);
+        }
+    }
+    return arr;
+ };
+
+ /*Array.prototype.filter = function (fn) {
+  return this.reduce((pre, val) => fn(val) ? [...pre, val] : pre, []);
+};*/
+
+/*
+Array.prototype.filter=function(fn){
+  return this.reduce((s,a)=>fn(a)?(s.push(a),s):s,[]);
+}
+*/
+
+/*
+Array.prototype.filter = function(callback) {
+	var result = [];
+  this.forEach(function(element) {
+  	if (callback(element))
+    	result.push(element);
+  });
+  return result;
+}
+*/
+
+/*
+Array.prototype.filter = function (cb) {
+  var result = [];
+
+  for(const el of this) {
+    if (cb(el)) {
+      result.push(el);
+    }
   }
+
+  return result;
+}
+*/
   
